36c5201bb96e99b0e0a70ecd6fc3bc87e25eff7d
Lucas Morais <lucas.morais23ti@gmail.com>
2023-09-20 15:48:27 -0300

    repository de pagamentos

---
+++ src/main/java/com/fadesppagamentos/repositories/PaymentRepository.java
@@ -0,0 +1,33 @@
+package com.fadesppagamentos.repositories;
+
+import com.fadesppagamentos.domain.payment.Payment;
+import com.fadesppagamentos.enumeration.PaymentStatusEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    // Encontre um pagamento por código de débito
+    Optional<Payment> findByCode(String code);
+
+    // Encontre pagamentos por CPF/CNPJ do pagador
+    List<Payment> findByDocument(String document);
+
+    // Encontre pagamentos pendentes de processamento
+    List<Payment> findByPaymentStatus(PaymentStatusEnum paymentStatusPending);
+
+    @Query("SELECT p FROM payments p " +
+            "WHERE (:code IS NULL OR p.code = :code) " +
+            "AND (:document IS NULL OR p.document = :document) " +
+            "AND (:paymentStatus IS NULL OR p.paymentStatus = :paymentStatus)")
+    List<Payment> findPaymentsByFilters(@Param("code") String code,
+                                        @Param("document") String document,
+                                        @Param("paymentStatus") PaymentStatusEnum paymentStatus);
+
+}
