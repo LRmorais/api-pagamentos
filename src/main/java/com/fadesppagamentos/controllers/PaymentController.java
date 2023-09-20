78b59e49fa9c42e79576c93caa9cf5ad637bf185
Lucas Morais <lucas.morais23ti@gmail.com>
2023-09-20 19:41:20 -0300

    ajustes gerais em todas as rotas, implementação da documentacao

--- src/main/java/com/fadesppagamentos/controllers/PaymentController.java
+++ src/main/java/com/fadesppagamentos/controllers/PaymentController.java
@@ -1,6 +1,6 @@
 package com.fadesppagamentos.controllers;
 
-import com.fadesppagamentos.domain.payment.Payment;
+import com.fadesppagamentos.dtos.PaymentDto;
 import com.fadesppagamentos.dtos.PaymentFormDto;
 import com.fadesppagamentos.enumeration.PaymentStatusEnum;
 import com.fadesppagamentos.services.PaymentService;
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/payments", produces = "application/json")
@@ -27,65 +26,79 @@ public class PaymentController {
     @Operation(summary = "Cria um pagamento", method = "POST")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Pagamento criado com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+            @ApiResponse(responseCode = "400", description = "Pagamento já processado ou em processamento"),
             @ApiResponse(responseCode = "500", description = "Erro interno")
     })
     @PostMapping(value = "/create", consumes = "application/json")
-    public ResponseEntity<Payment> createPayment(@RequestBody PaymentFormDto paymentFormDto) {
-        Payment createdPayment = paymentService.createPayment(paymentFormDto);
-        return new ResponseEntity<>(createdPayment, HttpStatus.CREATED);
+    public ResponseEntity<String> createPayment(@RequestBody PaymentFormDto paymentFormDto) {
+        String message = paymentService.createPayment(paymentFormDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(message);
     }
 
     @Operation(summary = "Busca um pagamento por id", method = "GET")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Pagamento encontrado"),
+            @ApiResponse(responseCode = "200", description = "Retorna os dados de pagamento"),
+            @ApiResponse(responseCode = "400", description = "Pagamento já processado"),
             @ApiResponse(responseCode = "404", description = "Pagamento não encontrado"),
             @ApiResponse(responseCode = "500", description = "Erro interno")
     })
     @GetMapping("/{id}")
-    public ResponseEntity<Payment> getPaymentById(@PathVariable Long id) {
-        Optional<Payment> payment = paymentService.getPaymentById(id);
-        return payment.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public ResponseEntity<PaymentDto> getPaymentById(@PathVariable Long id) {
+        PaymentDto payment = paymentService.getPaymentById(id);
+        return new ResponseEntity<>(payment, HttpStatus.OK);
     }
 
     @Operation(summary = "Busca todos os pagamentos", method = "GET")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Pagamentos encontrados"),
+            @ApiResponse(responseCode = "200", description = "retorna pagamentos encontrados"),
+            @ApiResponse(responseCode = "404", description = "Nenhum pagamento encontrado"),
             @ApiResponse(responseCode = "500", description = "Erro interno")
     })
     @GetMapping("/all")
-    public ResponseEntity<List<Payment>> getAllPayments() {
-        List<Payment> payments = paymentService.getAllPayments();
-        return new ResponseEntity<>(payments, HttpStatus.OK);
+    public ResponseEntity<List<PaymentDto>> getAllPayments() {
+        List<PaymentDto> paymentDtos = paymentService.getAllPayments();
+        return new ResponseEntity<>(paymentDtos, HttpStatus.OK);
     }
 
     @Operation(summary = "Busca todos os pagamentos de um usuário", method = "GET")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Pagamentos encontrados"),
+            @ApiResponse(responseCode = "404", description = "Nenhum pagamento encontrado"),
             @ApiResponse(responseCode = "500", description = "Erro interno")
     })
     @GetMapping("/filter")
-    public ResponseEntity<List<Payment>> filterPayments(@RequestParam(required = false) String code,
+    public ResponseEntity<List<PaymentDto>> filterPayments(@RequestParam(required = false) String code,
                                                         @RequestParam(required = false) String document,
                                                         @RequestParam(required = false) PaymentStatusEnum paymentStatus) {
-        List<Payment> filteredPayments = paymentService.filterPayments(code, document, paymentStatus);
+        List<PaymentDto> filteredPayments = paymentService.filterPayments(code, document, paymentStatus);
         return new ResponseEntity<>(filteredPayments, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Atualiza o status de um pagamento", method = "PATCH")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status do pagamento atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Pagamento não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<String> updatePaymentStatus(
+            @PathVariable Long id,
+            @RequestParam PaymentStatusEnum newStatus
+    ) {
+        String message = paymentService.updatePaymentStatus(id, newStatus);
+        return ResponseEntity.status(HttpStatus.OK).body(message);
     }
 
     @Operation(summary = "Deleta um pagamento", method = "DELETE")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Pagamento deletado com sucesso"),
+            @ApiResponse(responseCode = "200", description = "Pagamento deletado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Pagamento já processado"),
             @ApiResponse(responseCode = "404", description = "Pagamento não encontrado"),
             @ApiResponse(responseCode = "500", description = "Erro interno")
     })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePayment(@PathVariable Long id) {
-        boolean deleted = paymentService.deletePayment(id);
-        if (deleted) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<String> deletePayment(@PathVariable Long id) {
+        String message = paymentService.deletePayment(id);
+        return ResponseEntity.status(HttpStatus.OK).body(message);
     }
 }
