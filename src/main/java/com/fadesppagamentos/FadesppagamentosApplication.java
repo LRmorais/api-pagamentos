a8760038f7c55811d05a4b8775d37f2885cd7112
Lucas Morais <lucas.morais23ti@gmail.com>
2023-09-19 21:48:12 -0300

    first commit

---
+++ src/main/java/com/fadesppagamentos/FadesppagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fadesppagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FadesppagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FadesppagamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fadesppagamentos/FadesppagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadesppagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FadesppagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
